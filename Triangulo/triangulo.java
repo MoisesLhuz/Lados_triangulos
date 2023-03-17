6ed4c671e83751aa555033be2052b340b58c4ae5
Moises Santos da Luz <113149089+MoisesLhuz@users.noreply.github.com>
2023-03-17 15:02:55 -0300

    first_version

---
+++ Triangulo/triangulo.java
@@ -0,0 +1,30 @@
+package Triangulo;
+import java.util.Scanner;
+
+public class triangulo {
+    public static void main(String[] args) {
+        //declaração variáveis
+        
+        double a, b, c;
+        
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        System.out.println("Digite o Lado A do Triângulo");
+        a = teclado.nextDouble();
+        System.out.println("Digite o Lado B do Triângulo");
+        b = teclado.nextDouble();
+        System.out.println("Digite o Lado C do Triângulo");
+        c = teclado.nextDouble();
+        
+        //estrutura de decisão
+        if (a == b && a == c) {
+            System.out.println("Triângulo Equilátero");
+        } else if(((a == b) && (a != c)) || ((a == c) && (a != c))){
+            System.out.println("Triângulo Isósceles");
+        }else{
+            System.out.println("Triângulo Escaleno");
+        }
+        teclado.close();
+    }
+}
